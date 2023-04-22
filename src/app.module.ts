50193be978f25757422da6ba001f1fda042b48bd
leetaegyu <koallazon@gmail.com>
2023-04-22 09:21:12 +0900

    [Section9] 사용자 회원가입 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+const isDev = process.env.NODE_ENV === 'development';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
-      synchronize: true,
+      entities: [User, Report],
+      synchronize: isDev,
+      // 동기화 기능은 개발모드에서 사용해야 한다. production 모드에서 사용하면
+      // 동기화하는 과정에서 특정 컬럼이 제거되어 데이터를 잃을 수도 있기때문이다.
     }),
     UsersModule,
     ReportsModule,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    // 유효성 감사를 하기 위해 entity 인스턴스를 생성해서 저장한다.
+    return this.repo.save(user);
+  }
+}
