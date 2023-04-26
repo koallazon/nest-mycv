55e3171d22a7628f9c5496ed73fa7a17de46e19b
leetaegyu <koallazon@gmail.com>
2023-04-26 13:44:33 +0900

    [Section11] 인증 쿠키 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import { CookieSession } from 'cookie-session';
+// import * as cookieSession from 'cookie-session';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdfasfd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: false,
