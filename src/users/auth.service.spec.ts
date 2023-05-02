b3c4aa50de1a71bb20c91c3aa4ee5b78c064dfbb
leetaegyu <koallazon@gmail.com>
2023-05-02 13:47:34 +0900

    [Section12] 로그인 테스트

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,26 +2,61 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
-it('인증 서비스 인스턴스를 만들자', async () => {
-  // user service를 가짜로 복사해서 생성하자.
-  const fakeUsersServices: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthServiec', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersServices,
-      },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // user service를 가짜로 복사해서 생성하자.
+    fakeUsersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('인증 서비스 인스턴스를 만들자', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('암호화된 비밀번호되 새로운 사용자를 등록하자', async () => {
+    const user = await service.signup('asdf@asdf.com', 'asdf');
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+  it('throws an error if user signs up with email that is in use', async (done) => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    try {
+      await expect(
+        service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+      ).rejects.toThrow(NotFoundException);
+    } catch (err) {
+      done();
+    }
+  });
+
+  it('사용할 수 없는 이메일로 로그인을 했을 때 에러를 처리해야한다.', async (done) => {
+    try {
+      await service.signin('asdfasdf@nbasdlfk.com', '512dsfasfasedf');
+    } catch (ere) {
+      done();
+    }
+  });
 });
