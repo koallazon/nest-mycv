ce2b5c707eae3fcee1c9826e3684f75f67ca8dd9
ltg <ltg@bbangyanetworks.com>
2023-05-07 11:37:23 +0900

    [Section14] 환경변수 설정 및  e2e 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,34 @@
 import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: isDev,
-      // 동기화 기능은 개발모드에서 사용해야 한다. production 모드에서 사용하면
-      // 동기화하는 과정에서 특정 컬럼이 제거되어 데이터를 잃을 수도 있기때문이다.
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: !isProd,
+          // 동기화 기능은 개발모드에서 사용해야 한다. production 모드에서 사용하면
+          // 동기화하는 과정에서 특정 컬럼이 제거되어 데이터를 잃을 수도 있기때문이다.
+          entities: [User, Report],
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
