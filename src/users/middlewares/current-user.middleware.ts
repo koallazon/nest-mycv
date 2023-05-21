496586902c6598485c18febde6b00bde548b2b37
ltg <ltg@bbangyanetworks.com>
2023-05-21 11:24:38 +0900

    [Section18] 보고서 추정치 구하기

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      CurrentUser?: User;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddelware implements NestMiddleware {
@@ -8,18 +17,8 @@ export class CurrentUserMiddelware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
-    console.log(
-      '🚀 ~ file: current-user.middleware.ts:11 ~ CurrentUserMiddelware ~ use ~ userId:',
-      userId,
-    );
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      console.log(
-        '🚀 ~ file: current-user.middleware.ts:17 ~ CurrentUserMiddelware ~ use ~ user:',
-        user,
-      );
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       req.CurrentUser = user;
     }
     next();
