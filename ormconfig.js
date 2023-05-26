d622511c9699259e9684550cb20ae9aa9b9c07da
leetaegyu <koallazon@gmail.com>
2023-05-26 14:01:27 +0900

    [Section18] 환경별 typeorm 셋팅

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,27 @@
-module.exports = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities:
-    process.env.NODE_ENV === 'development'
-      ? ['**/*.entity.js']
-      : ['**/*.entity.ts'],
+var dbConfig = {
   synchronize: false,
 };
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    console.log('development');
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'production':
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
-const isProd = process.env.NODE_ENV === 'production';
 const cookieSession = require('cookie-session');
+const dbConfig = require('../ormconfig.js');
 
 @Module({
   imports: [
@@ -17,20 +15,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: !isProd,
-          // 동기화 기능은 개발모드에서 사용해야 한다. production 모드에서 사용하면
-          // 동기화하는 과정에서 특정 컬럼이 제거되어 데이터를 잃을 수도 있기때문이다.
-          entities: [User, Report],
-        };
-      },
-    }),
-
+    TypeOrmModule.forRoot(dbConfig),
     UsersModule,
     ReportsModule,
   ],
@@ -47,6 +32,7 @@ const cookieSession = require('cookie-session');
 })
 export class AppModule {
   constructor(private configService: ConfigService) {}
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
